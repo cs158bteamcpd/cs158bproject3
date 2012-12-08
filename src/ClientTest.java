a82afc1d096f45355c47e1f53eea0c01737bbfd5
darkblade25 <darkblade25@gmail.com>
2012-12-07 22:59:51 -0800

    Added GET and SET and a simple demo of it

---
+++ src/ClientTest.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Hashtable;
+
+
+public class ClientTest {
+	public static void main(String[]args){
+		try {
+			Socket s = new Socket("localhost", 9999);
+			
+			SNMP snmp = null;
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		    
+			System.out.print("Enter oid: '");
+			
+			String oid = in.readLine();
+			
+			
+			
+			System.out.print("Enter community String: '");
+			
+			String community = in.readLine();
+			
+			System.out.print("Get Or Set: '");
+			
+			String getOrSet = in.readLine();
+			
+			if( getOrSet.equalsIgnoreCase("get")){
+			
+				Hashtable<String,String> ht = new Hashtable<String,String>();
+				
+				ht.put(oid, oid);
+				
+				snmp = new SNMP("1",community,"1","GET", ht);
+			} else {
+				
+				System.out.print("Enter New Value: '");
+				
+				String value = in.readLine();
+				
+				Hashtable<String,String> ht = new Hashtable<String,String>();
+				
+				ht.put(oid, value);
+				
+				snmp = new SNMP("1",community,"1","SET", ht);
+				
+			}
+			
+			
+			ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+			
+			
+			oos.writeObject(snmp);
+			
+			
+			//Now Wait for response
+			
+			ObjectInputStream ois = new ObjectInputStream(s.getInputStream());	
+			
+			SNMP response = (SNMP)ois.readObject();
+			
+			System.out.println(response.vBinding.get(oid));
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Unknown Host");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error retreving data");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+}
--- src/SNMP.java
+++ src/SNMP.java
@@ -4,27 +4,68 @@ import java.util.Hashtable;
 
 public class SNMP implements Serializable
 {
- /**
-	 * 
-	 */
+	 
 	private static final long serialVersionUID = -2084003647637684925L;
-public Hashtable<String,String> vBinding; // the variable binding, OID and value of MIB object
- public String pdutype = "RESPONSE";
+	public Hashtable<String,String> vBinding; // the variable binding, OID and value of MIB object
+	public String pdutype = "RESPONSE";
+	
+	private String community; // the community string
 
- public String community; // the community string
- public String host; // the host ID
- public String version; //version of SNMP
-  
- public SNMP(String hostID, String communityStr, String versionSNMP, String pdutype, Hashtable<String,String> variableBinding)
- {
- 	//OID = objectID;
- 	//value = vvalue;
- 	this.pdutype = pdutype;
- 	vBinding = variableBinding;
- 	community = communityStr;
- 	host = hostID;
- 	version = versionSNMP;
- }
+	private String host; // the host ID
+	private String version; //version of SNMP
+	  
+	 public SNMP(String hostID, String communityStr, String versionSNMP, String pdutype, Hashtable<String,String> variableBinding)
+	 {
+		//OID = objectID;
+		//value = vvalue;
+	 	this.pdutype = pdutype;
+	 	vBinding = variableBinding;
+	 	community = communityStr;
+	 	host = hostID;
+	 	version = versionSNMP;
+	}
+	
+ 
+	
+	public Hashtable<String, String> getvBinding() {
+		return vBinding;
+	}
 
- //missing snmp traps
+	public void setvBinding(Hashtable<String, String> vBinding) {
+		this.vBinding = vBinding;
+	}
+
+	public String getPdutype() {
+		return pdutype;
+	}
+
+	public void setPdutype(String pdutype) {
+		this.pdutype = pdutype;
+	}
+
+	public String getCommunity() {
+		return community;
+	}
+
+	public void setCommunity(String community) {
+		this.community = community;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	//missing snmp traps
 }
